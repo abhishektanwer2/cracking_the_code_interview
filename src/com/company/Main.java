bbbf09fa6508bbf368730ade5fa25de8204deb1e
Abhishek Tanwer <abhishektanwer2@gmail.com>
2020-09-30 13:54:16 -0400

    new question solutions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String inputstring="abcda";
+        System.out.print(isUniquechars(inputstring));
+    }
+    public static boolean isUniquechars(String inputstring){
+        boolean []unique=new boolean[128];
+        if(inputstring.length()>128)return false;
+        for(int i=0;i<inputstring.length();i++){
+            if(unique[inputstring.charAt(i)-'a'])return false;
+            else{
+                unique[inputstring.charAt(i)-'a']=true;
+            }
+        }
+        return true;
     }
 }
